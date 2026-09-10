0bddcc337e755568f8cb99b7547fdffe65b044e9
Eduardo Cristofari <eduardocristofariguterres@gmail.com>
2026-09-10 10:38:40 -0300

    Ajust avatar dark

--- script.js
+++ script.js
@@ -1,17 +1,11 @@
 function toggleMode() {
   const html = document.documentElement
-  /* if (html.classList.contains("light")) {
-    html.classList.remove("light")
-  } else {
-    html.classList.add("light")
-  } */
   html.classList.toggle("light")
-  //pegar a imagem
-  const img = document.querySelector("#profile img ")
-  // substituir a imgame
+  const img = document.querySelector("#profile img")
+
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar2.png")
+    img.src = "./assets/avatar2.png"
   } else {
-    img.setAttribute("src", "./assets/avatar.png")
+    img.src = "./assets/avatar.png"
   }
 }
